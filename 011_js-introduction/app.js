fdb725c99442e9aa8dd8e71477036f89d01443df
n3o <the3n3o@gmail.com>
2024-07-02 14:11:40 +0900

    Day 30
    Start learning JS

---
+++ 011_js-introduction/app.js
@@ -0,0 +1,18 @@
+let age = 32;
+let userName = "Max";
+let hobbies = ["Sports", "Cooking", "Reading", 1, 2];
+/* arrays */
+let job = { title: 'Developer', place: 'New York', salary: 50000 };
+/* 
+object (객체)
+{} 안에 레이블들을 정해주고 : 뒤에 value를 넣어줌
+*/
+
+/* 
+alert(hobbies);
+모든 arrays를 다 가져옴
+*/
+alert(hobbies[0]);
+/* arrays의 인덱스는 순서대로 0부터 가지고 있음  */
+alert(job.place);
+/* obj.value */
