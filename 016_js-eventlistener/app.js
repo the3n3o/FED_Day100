e3450a023c9cd5f49ddf277e97453d7ba53824a3
n3o <the3n3o@gmail.com>
2024-07-28 12:50:16 +0900

    Day 35_3

--- 016_js-eventlistener/app.js
+++ 016_js-eventlistener/app.js
@@ -1,17 +1,20 @@
 let paragraphElement = document.querySelector("p");
 
-function changeParagraphText() {
+function changeParagraphText(event) {
     paragraphElement.textContent = "Clicked !";
     console.log('Paragraph Clicked!')
+    console.log(event);
 }
 
 paragraphElement.addEventListener("click", changeParagraphText);
 
 let inputTextElement = document.querySelector('input');
 
-function  retrieveUserInput() {
-    let enteredText = inputTextElement.value;
+function  retrieveUserInput(event) {
+    /* let enteredText = inputTextElement.value; */
+    let enteredText = event.target.value
     console.log(enteredText);
+    console.log(event);
 }
 
 inputTextElement.addEventListener('input',retrieveUserInput);
