0e4cc6d4144a31d9fe655c7dccd5b4e60d639e3f
n3o <the3n3o@gmail.com>
2024-07-28 19:33:52 +0900

    Revert "Day 36_2"
    This reverts commit 8cc9b20a89faec91d76f6d05e738a0cbf4220172.

--- 017_js-eventlistener-exercise/app.js
+++ 017_js-eventlistener-exercise/app.js
@@ -1,28 +1,15 @@
-const inputCharsElement = document.querySelector('#productname');
+let inputCharsElement = document.querySelector('#productname');
 
-const remainingCharsElement = document.querySelector('#remaining-chars');
+let remainingCharsElement = document.querySelector('#remaining-chars');
 
-const maxAllowedChars = inputCharsElement.maxLength;
+let maxAllowedChars = inputCharsElement.maxLength;
 
 function changeRemainingChars(event) {
-    const enteredText = event.target.value;
-    const enteredTextLength = enteredText.length;
-    const remainingChars = maxAllowedChars - enteredTextLength;
+    let enteredText = event.target.value;
+    let enteredTextLength = enteredText.length;
+    let remainingChars = maxAllowedChars - enteredTextLength;
 
     remainingCharsElement.textContent = remainingChars;
 }
 
 inputCharsElement.addEventListener('input',changeRemainingChars);
-
-// JS 로 Style
-
-//remainingCharsElement.style.backgroundColor = 'grey';
-
-/* inputCharsElement.className = 'warning';
-remainingCharsElement.className = 'warning'; */
-// 이 방법은 기존 class가 있다면 overwrite 하는 단점이 있음.
-// 그래서 아래와 같은 방법을 추천함
-
-inputCharsElement.classList.add('warning');
-remainingCharsElement.classList.add('warning');
-// 빼는 방법은 .remove
