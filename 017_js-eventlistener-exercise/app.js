2dbee2a12360997a46ff7041a8dfe7bf9537ae72
n3o <the3n3o@gmail.com>
2024-07-28 19:26:18 +0900

    Day 36_2

--- 017_js-eventlistener-exercise/app.js
+++ 017_js-eventlistener-exercise/app.js
@@ -1,15 +1,28 @@
-let inputCharsElement = document.querySelector('#productname');
+const inputCharsElement = document.querySelector('#productname');
 
-let remainingCharsElement = document.querySelector('#remaining-chars');
+const remainingCharsElement = document.querySelector('#remaining-chars');
 
-let maxAllowedChars = inputCharsElement.maxLength;
+const maxAllowedChars = inputCharsElement.maxLength;
 
 function changeRemainingChars(event) {
-    let enteredText = event.target.value;
-    let enteredTextLength = enteredText.length;
-    let remainingChars = maxAllowedChars - enteredTextLength;
+    const enteredText = event.target.value;
+    const enteredTextLength = enteredText.length;
+    const remainingChars = maxAllowedChars - enteredTextLength;
 
     remainingCharsElement.textContent = remainingChars;
 }
 
 inputCharsElement.addEventListener('input',changeRemainingChars);
+
+// JS 로 Style
+
+//remainingCharsElement.style.backgroundColor = 'grey';
+
+/* inputCharsElement.className = 'warning';
+remainingCharsElement.className = 'warning'; */
+// 이 방법은 기존 class가 있다면 overwrite 하는 단점이 있음.
+// 그래서 아래와 같은 방법을 추천함
+
+inputCharsElement.classList.add('warning');
+remainingCharsElement.classList.add('warning');
+// 빼는 방법은 .remove
