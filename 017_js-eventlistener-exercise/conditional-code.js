a189f331a78d2b8edb16e20cd4e7cbd09946a8d6
n3o <the3n3o@gmail.com>
2024-08-01 19:27:27 +0900

    Day 37_3

---
+++ 017_js-eventlistener-exercise/conditional-code.js
@@ -0,0 +1,30 @@
+const myName = 'Neo';
+
+if (true) {
+    console.log('hello');
+}
+// if (부울값)
+
+// 5 == 5 true
+// 5 == '5' true 
+// 5 === '5' false 
+
+// 5 != 5 flase
+// 5 != '5' false
+// 5 !== 5 false
+// 5 !== '5' true
+
+// 3 > 4 false
+// 'a' > 'b' false
+// 'cb' > 'ab' ture 
+    // 글자별로 비교, 'c' > 'a' 
+    // 만약 'ac' > 'ab' 라면 
+    // 'a' 는 같으니 2번째 글자인 'c' > 'b' 
+    // 즉 true 
+
+// 5 >= 5 true
+// 3 <= 2 false
+// !(5 < 5) true
+// !(5 <= 5) false
+
+// && [and]   || [or]
